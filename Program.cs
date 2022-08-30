0208ee21869db4ad14e71c0d3152881d59486a4f
EVGEN <mtmsys4@gmail.com>
2022-08-30 08:18:01 +0300

    Черновое решение задачи 2. Работает

--- Program.cs
+++ Program.cs
@@ -79,6 +79,22 @@ YesOrNo(num);
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+double xA = 7;
+double yA = -5;
+double zA = 0;
+
+double xB = 1;
+double yB = -1;
+double zB = 9;
+
+double ac   = yA - yB ;
+double bc  = Math.Sqrt ((xA - xB) * (xA - xB) + (zA - zB)*(zA - zB));
+double ab  = Math.Sqrt ((ac*ac) + (bc*bc));
+
+
+Console.WriteLine(ab);
 
 
 
